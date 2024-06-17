e3e60b9e62736128ee430bac0a7977b7cc68e755
vedantgaikar <vedantgaikar14@gmail.com>
2024-06-17 21:06:31 +0530

    Modified the styledText.dart to accept custom string to display

--- lib/styledText.dart
+++ lib/styledText.dart
@@ -2,14 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText( this.text, {super.key});
+  final String text;
 
   @override
   Widget build(BuildContext context) {
 
     // TODO: implement build
     return Text(
-      'Hello World',
+      text,
       style: TextStyle(color: Colors.white, fontSize: 30,),
     );
   }
